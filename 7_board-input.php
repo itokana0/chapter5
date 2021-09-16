43f932f82a79750b668ab9b2bcd455693219001b
itokana0 <itodenwa123@gmail.com>
2021-09-16 19:08:25 +0900

    inicial commit

---
+++ 7_board-input.php
@@ -0,0 +1,7 @@
+<?php require '../header.php';?>
+<p>投稿するメッセージを入力してください。</p>
+<form action="7_board-output.php">
+<input type="text" name="message">
+<input type="submit" value="投稿">
+</form>
+<?php require '../footer.php';?>
